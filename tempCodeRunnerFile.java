27c53436b5b0df77b14d85603147d8ab525672db
raahulpandey <arryaroy7367@gmail.com>
2025-01-30 12:42:09 +0530

    hii devopers

--- tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-class Array1 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int num[]=new int[size];
